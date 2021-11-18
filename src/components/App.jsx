5bc7c0001d11f2ff11bf16de920c101bf8696ddb
nayan1xyz <nayan2xyznayan@gmail.com>
2021-11-18 19:53:46 +0530

    Updated Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-
+import Page from "./Page";
   function App()
   {
   return (
     <div className="App">
       
+      <Page /> 
     </div>
+    
+
+    
   );
 }
 
